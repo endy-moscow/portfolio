fbb727ace93be585001bd320eb8ca3a2591f56b3
endy-moscow <endy124@gmail.com>
2018-03-04 21:03:49 +0300

    add Swiper prototype

--- client/components/article/article.jsx
+++ client/components/article/article.jsx
@@ -1,35 +1,43 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Swiper from 'react-id-swiper';
+import Swiper from 'react-id-swiper'
 
 export default class Article extends React.Component {
   render() {
     const params = {
+          ContainerEl: 'section',
+          WrapperEl: 'section',
           navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev'
+            prevEl: '.swiper-button-prev',
+            nextEl: '.swiper-button-next'
           },
-          pagination: {
-            el: '.swiper-pagination',
-            type: 'progressbar',
-          },
+          grabCursor: true,
         }
     return (
       <article>
         <div className='content_container'>
           <div className='article'>
             <h2>Title</h2>
+            <p className='article_txt'>Давно выяснено, что при оценке дизайна и композиции читаемый текст мешает сосредоточиться. Lorem Ipsum используют потому, что тот обеспечивает более или менее стандартное заполнение шаблона, а также реальное распределение букв и пробелов в абзацах, которое не получается при простой дубликации "Здесь ваш текст.. Здесь ваш текст.. Здесь ваш текст.." Многие программы электронной вёрстки и редакторы HTML используют Lorem Ipsum в качестве текста по умолчанию, так что поиск по ключевым словам lorem ipsum сразу показывает, как много веб-страниц всё ещё дожидаются своего настоящего рождения. За прошедшие годы текст Lorem Ipsum получил много версий. Некоторые версии появились по ошибке, некоторые - намеренно (например, юмористические варианты).
+            </p>
         </div>
         <Swiper {...params}>
-          <div className='cola2015-img1'>Slide 1</div>
-          <div className='cola2015-img2'>Slide 2</div>
-          <div className='cola2015-img3'>Slide 3</div>
-          <div className='cola2015-img4'>Slide 4</div>
+          <div className='cola2015-img1'>
+            <div className='photo-description'>Slide</div>
+          </div>
+          <div className='cola2015-img1'>
+            <div className='photo-description'>Slide</div>
+          </div>
+          <div className='cola2015-img1'>
+            <div className='photo-description'>Slide</div>
+          </div>
+          <div className='cola2015-img1'>
+            <div className='photo-description'>Slide</div>
+          </div>
         </Swiper>
         <div className='content_container'>
             <h2>Title</h2>
-            <div className='article_txt'>Давно выяснено, что при оценке дизайна и композиции читаемый текст мешает сосредоточиться. Lorem Ipsum используют потому, что тот обеспечивает более или менее стандартное заполнение шаблона, а также реальное распределение букв и пробелов в абзацах, которое не получается при простой дубликации "Здесь ваш текст.. Здесь ваш текст.. Здесь ваш текст.." Многие программы электронной вёрстки и редакторы HTML используют Lorem Ipsum в качестве текста по умолчанию, так что поиск по ключевым словам lorem ipsum сразу показывает, как много веб-страниц всё ещё дожидаются своего настоящего рождения. За прошедшие годы текст Lorem Ipsum получил много версий. Некоторые версии появились по ошибке, некоторые - намеренно (например, юмористические варианты).
-            </div>
+
           </div>
         </div>
       </article>
